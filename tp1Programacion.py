0db79724c3388891f50b436f7143bfcefca181ce
RodPC <rodri_08romero@hotmail.com>
2023-08-16 16:03:53 -0300

    Primer Commit

---
+++ tp1Programacion.py
@@ -0,0 +1,38 @@
+#Ejercicio 1
+numero1=5
+#Ejercicio 2
+numero2=10.5
+#Ejercicio 3
+suma=(numero1+numero2)
+#Ejercicio 4
+resta=(numero1-numero2)
+mult=(numero1*numero2)
+division=(numero1/numero2)
+print(f"Los numeros ingresados fueron {numero1} y {numero2}. \n La suma de ambos es {suma} \n La resta de ambos es {resta} \n La multiplicacion de ambos es {mult} \n La division de ambos es {division} ")
+#Ejercicio 5
+nombre="Rodrigo"
+#Ejercicio 6
+precio=(499.99)
+#Ejercicio 7
+descuento=0.25
+#Ejercicio 8
+precio_final=(precio*(1.00-descuento))
+#Ejercicio 9
+cadena="No todo lo que brilla es oro"
+#Ejercicio 10
+longitud=len(cadena)
+#Ejercicio 11
+precio_entero=int(precio)
+#Ejercicio 12
+apellido="Romero"
+nombre_completo=(nombre+" "+apellido)
+print(nombre_completo)
+#Ejercicio 13
+edad=(20+5-10)
+#Ejercicio 14
+altura=((1.88*4)/10)
+#Ejercicio 15
+nombre_mayus="RODRIGO ROMERO"
+nombre_minus=(nombre_mayus.lower())
+#Ejercicio 16
+print(nombre_mayus.capitalize())
